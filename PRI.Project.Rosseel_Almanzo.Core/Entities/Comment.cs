f0d72fcf456394e5aa650bce1ec72f9d74afb98b
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-05 15:45:39 +0100

    Edit needed properties for entity comment

--- PRI.Project.Rosseel_Almanzo.Core/Entities/Comment.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/Comment.cs
@@ -2,6 +2,14 @@
 {
     public class Comment
     {
-        
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+        public Route Route { get; set; }
+        public int RouteId { get; set; }
+        public Event Event { get; set; }
+        public int EventId { get; set; }
     }
 }
