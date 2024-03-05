b2f9b30f384f5b46b23d83d2691040c9f7cbdc71
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-05 15:47:02 +0100

    Edit needed properties for entity user

--- PRI.Project.Rosseel_Almanzo.Core/Entities/User.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/User.cs
@@ -14,6 +14,20 @@ namespace PRI.Project.Rosseel_Almanzo.Core.Entities
 {
     public class User
     {
-        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public Address Address { get; set; }
+        public int AddressId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public ICollection<Dog> Dogs { get; set; }
+        public ICollection<Route> Routes { get; set; }
+        public ICollection<Event> OrganizedEvents { get; set; }
+        public ICollection<Event> AttendingEvents { get; set; }
+        public ICollection<Image> Images { get; set; }
     }
 }
