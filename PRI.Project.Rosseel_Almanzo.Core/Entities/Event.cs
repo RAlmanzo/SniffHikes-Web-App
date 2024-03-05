4a7b37a8b9ecc21b5d466d3607046d9afd5be4da
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-05 16:55:09 +0100

    Add property price to event

--- PRI.Project.Rosseel_Almanzo.Core/Entities/Event.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/Event.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public decimal Price { get; set; }
         public Address Address { get; set; }
         public ICollection<Image> Images { get; set; }
         public DateTime Date { get; set; }
