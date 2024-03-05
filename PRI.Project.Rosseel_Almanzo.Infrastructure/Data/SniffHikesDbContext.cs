664740eefd12fcdc2869d84059cc50785b54b05d
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-05 16:36:00 +0100

    Create sniffhikesdbcontext

---
+++ PRI.Project.Rosseel_Almanzo.Infrastructure/Data/SniffHikesDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Infrastructure.Data
+{
+    public class SniffHikesDbContext : DbContext
+    {
+        public SniffHikesDbContext(DbContextOptions<SniffHikesDbContext> 
+            options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            //Seeder.Seed(modelBuilder);
+        }
+    }
+}
