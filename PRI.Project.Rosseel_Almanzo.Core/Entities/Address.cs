a5d07ee6b3842f1f708b22a140a6d44d68575ca4
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-05 15:45:16 +0100

    Edit needed properties for address entity

--- PRI.Project.Rosseel_Almanzo.Core/Entities/Address.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/Address.cs
@@ -2,6 +2,11 @@
 {
     public class Address
     {
-        
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        //public int ZipCode { get; set; }
     }
 }
