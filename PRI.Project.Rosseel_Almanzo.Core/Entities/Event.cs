9d5dbc58ef74bbe18a6f4a955ef9e1d3a56cb22f
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-05 15:46:17 +0100

    Edit needed properties for entity event

--- PRI.Project.Rosseel_Almanzo.Core/Entities/Event.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/Event.cs
@@ -2,6 +2,16 @@
 {
     public class Event
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Address Address { get; set; }
+        public ICollection<Image> Images { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime DateCreated { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public User Organizer { get; set; }
+        public int OrganizerId { get; set; }
+        public ICollection<User> AttendingUsers { get; set; }
     }
 }
