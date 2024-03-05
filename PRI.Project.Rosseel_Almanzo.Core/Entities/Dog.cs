551947c113facaa60e8631f71fa9b273a123b32d
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-05 15:45:57 +0100

    Edit needed properties for entity dog

--- PRI.Project.Rosseel_Almanzo.Core/Entities/Dog.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/Dog.cs
@@ -2,6 +2,13 @@
 {
     public class Dog
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Image { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
     }
 }
