4a2557675472cdd3d991d0c61ded4e61f7583093
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-05 15:46:48 +0100

    Edit needed properties for entity route

--- PRI.Project.Rosseel_Almanzo.Core/Entities/Route.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/Route.cs
@@ -2,6 +2,14 @@
 {
     public class Route
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Address Address { get; set; }
+        public ICollection<Image> Images { get; set; }
+        public DateTime DateCreated { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
     }
 }
