18c4fb70a07dfceff07efc4ab50e4e2a4f41bacc
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 18:21:39 +0100

    Edit code to delete an selected event

--- PRI.Project.Rosseel_Almanzo.Core/Services/EventService.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Services/EventService.cs
@@ -12,9 +12,39 @@ namespace PRI.Project.Rosseel_Almanzo.Core.Services
 {
     public class EventService : IEventService
     {
-        public Task<ResultModel<Event>> DeleteRecordAsync(int id)
+        private readonly IEventRepository _eventRepository;
+
+        public EventService(IEventRepository eventRepository)
         {
-            throw new NotImplementedException();
+            _eventRepository = eventRepository;
+        }
+
+        public async Task<ResultModel<Event>> DeleteEventAsync(int id)
+        {
+            //get the event
+            var selectedEvent = await _eventRepository.GetByIdAsync(id);
+
+            //check iff event exists
+            if (selectedEvent == null)
+            {
+                return new ResultModel<Event>
+                {
+                    Success = false,
+                    Errors = new List<string> { "Event does not exist!" }
+                };
+            }
+
+            //check if deleteAsync returns true
+            if(await _eventRepository.DeleteAsync(selectedEvent))
+            {
+                return new ResultModel<Event> { Success = true, };
+            }
+            //if not
+            return new ResultModel<Event>
+            {
+                Success = false,
+                Errors = new List<string> { "Some error occured!" }
+            };
         }
 
         public Task<ResultModel<IEnumerable<Event>>> GetAllAsync()
