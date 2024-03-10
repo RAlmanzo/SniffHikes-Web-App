3e2a7e6e04aa74d48eef52bfa91342c56ecea438
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 18:13:22 +0100

    Edit small name changes in service interfaces

--- PRI.Project.Rosseel_Almanzo.Core/Interfaces/Services/IEventService.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Interfaces/Services/IEventService.cs
@@ -15,6 +15,6 @@ namespace PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services
         Task<ResultModel<Event>> GetByIdAsync(Guid id);
         //Task<ResultModel<Event>> CreateRecordAsync(EventCreateRequestModel eventCreateRequestModel);
         //Task<ResultModel<Event>> UpdateRecordAsync(EventUpdateRequestModel eventUpdateRequestModel);
-        Task<ResultModel<Event>> DeleteRecordAsync(int id);
+        Task<ResultModel<Event>> DeleteEventAsync(int id);
     }
 }
