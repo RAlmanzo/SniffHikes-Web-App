c9d9005c92def949c8e94fb634c8334ed80de012
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 20:16:30 +0100

    Create eventsgetresponsedto

---
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/EventsGetResponseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
+{
+    public class EventsGetResponseDto : BaseDto
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime DateCreated { get; set; }
+        public BaseDto Orginazer { get; set; }
+        public BaseDto Address { get; set; }
+        public IEnumerable<BaseDto> Images { get; set; }
+        public IEnumerable<BaseDto> Comments { get; set; }
+        public IEnumerable<BaseDto> Users { get; set; }
+    }
+}
