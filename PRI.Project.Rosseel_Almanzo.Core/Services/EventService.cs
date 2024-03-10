47039db13e601c8b9254bc0f8529e5e0a3b8fa52
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 18:08:14 +0100

    Create class eventservice and implement interface ieventservice

---
+++ PRI.Project.Rosseel_Almanzo.Core/Services/EventService.cs
@@ -0,0 +1,30 @@
+﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Repositories;
+using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services;
+using PRI.Project.Rosseel_Almanzo.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Services
+{
+    public class EventService : IEventService
+    {
+        public Task<ResultModel<Event>> DeleteRecordAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<IEnumerable<Event>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<Event>> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
