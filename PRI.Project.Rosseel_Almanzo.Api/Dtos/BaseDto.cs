cac12387c08e0d0b5f7ab9d28611394a48ca3283
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 19:39:12 +0100

    Create basedto and eventsgetallresponsedto

---
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/BaseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
+{
+    public class BaseDto
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/EventsGetAllResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
+{
+    public class EventsGetAllResponseDto
+    {
+        public IEnumerable<BaseDto> Events { get; set; }
+    }
+}
