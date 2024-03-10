9919ef8f3a1afde105dac34655b2f4e23abb8dad
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 18:54:27 +0100

    Create eventscontroller and set attributes for apicontroller

---
+++ PRI.Project.Rosseel_Almanzo.Api/Controllers/EventsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
