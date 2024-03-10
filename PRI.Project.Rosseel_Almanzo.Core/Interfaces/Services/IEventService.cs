8762f515ca980dca8e4f65be0ca41bd3e7e45a11
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 17:45:07 +0100

    Create ieventservice interface

---
+++ PRI.Project.Rosseel_Almanzo.Core/Interfaces/Services/IEventService.cs
@@ -0,0 +1,16 @@
+﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using PRI.Project.Rosseel_Almanzo.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services
+{
+    public interface IEventService
+    {
+        Task<ResultModel<IEnumerable<Event>>> GetAllAsync();
+    }
+}
