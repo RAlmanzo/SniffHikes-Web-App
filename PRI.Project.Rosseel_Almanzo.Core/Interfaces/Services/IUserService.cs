9138bb86a126361ba41b98813f411a5c75ba23c9
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 17:59:13 +0100

    Edit methods that needs to be implementen with iuserservice

---
+++ PRI.Project.Rosseel_Almanzo.Core/Interfaces/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using PRI.Project.Rosseel_Almanzo.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<ResultModel<IEnumerable<User>>> GetAllAsync();
+        Task<ResultModel<User>> GetByIdAsync(Guid id);
+        //Task<ResultModel<User>> CreateRecordAsync(EventCreateRequestModel eventCreateRequestModel);
+        //Task<ResultModel<User>> UpdateRecordAsync(EventUpdateRequestModel eventUpdateRequestModel);
+        Task<ResultModel<User>> DeleteRecordAsync(int id);
+    }
+}
