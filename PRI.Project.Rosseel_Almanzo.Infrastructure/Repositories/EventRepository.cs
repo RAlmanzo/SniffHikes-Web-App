c03ab5ab470175f4be5660d0df7ccefd21512e42
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 17:12:43 +0100

    Let eventrepository inherit from baserepository and create constructor

---
+++ PRI.Project.Rosseel_Almanzo.Infrastructure/Repositories/EventRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using PRI.Project.Rosseel_Almanzo.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Infrastructure.Repositories
+{
+    public class EventRepository : BaseRepository<Event>
+    {
+        public EventRepository(SniffHikesDbContext context, ILogger<BaseRepository<Event>> logger) 
+            : base(context, logger)
+        {
+        }
+    }
+}
