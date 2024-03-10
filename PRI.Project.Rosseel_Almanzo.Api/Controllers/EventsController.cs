8817438d10c28d674f611ae015501981952af3f9
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 18:56:54 +0100

    Set up depency injection

--- PRI.Project.Rosseel_Almanzo.Api/Controllers/EventsController.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Controllers/EventsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services;
 
 namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
 {
@@ -6,6 +7,17 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
     [ApiController]
     public class EventsController : Controller
     {
+        private readonly IEventService _eventService;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<EventsController> _logger;
+
+        public EventsController(IEventService eventService, IWebHostEnvironment webHostEnvironment, ILogger<EventsController> logger)
+        {
+            _eventService = eventService;
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
