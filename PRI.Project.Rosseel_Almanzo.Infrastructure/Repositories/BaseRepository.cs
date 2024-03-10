40818000c815e5095650ddeff9683dc67032bf6b
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-10 16:09:41 +0100

    Create new class baserepository and implement interface ibaserepository

---
+++ PRI.Project.Rosseel_Almanzo.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T>
+    {
+        public Task<bool> AddAsync(T toAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(T toDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(T toUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
