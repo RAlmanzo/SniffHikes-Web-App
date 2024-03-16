f1578a7ed67d082fc24cfd6b5ee85c483beb958c
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-16 22:46:52 +0100

    Set navigation ids nullable

--- PRI.Project.Rosseel_Almanzo.Core/Entities/Comment.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/Comment.cs
@@ -6,10 +6,10 @@
         public string Content { get; set; }
         public DateTime DateCreated { get; set; }
         public User User { get; set; }
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
         public Route Route { get; set; }
-        public int RouteId { get; set; }
+        public int? RouteId { get; set; }
         public Event Event { get; set; }
-        public int EventId { get; set; }
+        public int? EventId { get; set; }
     }
 }
