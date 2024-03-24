dad8f11d2cf5d4955743f3b52cc9634dd7c7c32c
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-24 17:52:59 +0100

    Create new dto eventrequestdto

---
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/EventRequestDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
+{
+    public class EventRequestDto
+    {
+        [Required(ErrorMessage = "Title missing")]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Range(0.0, int.MaxValue)]
+        public decimal Price { get; set; }
+        //[Required]
+        public int AddressId { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        public DateTime DateCreated { get; set; }
+        //[Required]
+        public int OrganizerId { get; set; }
+        public IEnumerable<int> ImageIds { get; set; }    
+        public IEnumerable<int> CommentIds { get; set; }     
+        public IEnumerable<int> AttendingUserIds { get; set; }
+    }
+}
