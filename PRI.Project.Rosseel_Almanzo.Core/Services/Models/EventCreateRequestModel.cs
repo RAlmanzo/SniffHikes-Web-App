d907c0b54d57dae6537b06a1b6cf17da9f82e067
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-24 17:53:38 +0100

    Create new model eventcreaterequestmodel

---
+++ PRI.Project.Rosseel_Almanzo.Core/Services/Models/EventCreateRequestModel.cs
@@ -0,0 +1,23 @@
+﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Services.Models
+{
+    public class EventCreateRequestModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int AddressId { get; set; }
+        public IEnumerable<int> ImageIds { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime DateCreated { get; set; }
+        public IEnumerable<int> CommentIds { get; set; }
+        public int OrganizerId { get; set; }
+        public IEnumerable<int> AttendingUserIds { get; set; }
+    }
+}
