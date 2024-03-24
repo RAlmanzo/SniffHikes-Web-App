70644a4fd14bb7744ec68345ead05e61898ca02f
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-24 21:43:42 +0100

    Refacter entities image and address

--- PRI.Project.Rosseel_Almanzo.Core/Entities/Image.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/Image.cs
@@ -6,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace PRI.Project.Rosseel_Almanzo.Core.Entities
 {
-    public class Image
+    public class Image : BaseEntity
     {
-        public int Id { get; set; }
         public User User { get; set; }
         public int? UserId { get; set; }
         public string File { get; set; }
