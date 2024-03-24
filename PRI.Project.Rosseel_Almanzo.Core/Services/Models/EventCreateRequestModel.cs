8b97f0e3430b9dafe96b96efed253712ba5b2c1a
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-24 21:46:09 +0100

    Refacter properties f eventcreaterrequestmodel

--- PRI.Project.Rosseel_Almanzo.Core/Services/Models/EventCreateRequestModel.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Services/Models/EventCreateRequestModel.cs
@@ -12,12 +12,15 @@ namespace PRI.Project.Rosseel_Almanzo.Core.Services.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public int AddressId { get; set; }
-        public IEnumerable<int> ImageIds { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public int OrganizerId { get; set; }
         public DateTime Date { get; set; }
         public DateTime DateCreated { get; set; }
-        public IEnumerable<int> CommentIds { get; set; }
-        public int OrganizerId { get; set; }
-        public IEnumerable<int> AttendingUserIds { get; set; }
+        //public IEnumerable<int> ImageIds { get; set; }     
+        //public IEnumerable<int> CommentIds { get; set; }
+        //public IEnumerable<int> AttendingUserIds { get; set; }
     }
 }
