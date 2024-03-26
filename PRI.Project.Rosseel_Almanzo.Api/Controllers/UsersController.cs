a59af3f575817404a41d3d1da321c0536eb6c0ed
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-26 15:38:39 +0100

    Create new api controller userscontroller and add depency injection

---
+++ PRI.Project.Rosseel_Almanzo.Api/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services;
+
+namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<EventsController> _logger;
+
+        public UsersController(IUserService userService, IWebHostEnvironment webHostEnvironment, ILogger<EventsController> logger)
+        {
+            _userService = userService;
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+
+        
+    }
+}
