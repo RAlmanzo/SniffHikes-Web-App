a205fac9d9bc544ac235ddb2f0302fd0dd9c366f
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-26 21:11:27 +0100

    Set code in comment for testing purpose

--- PRI.Project.Rosseel_Almanzo.Api/Dtos/EventRequestDto.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/EventRequestDto.cs
@@ -19,8 +19,8 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
         public int OrganizerId { get; set; }
         [Required]
         public Address Address { get; set; }
-        public IEnumerable<int> ImageIds { get; set; }    
-        public IEnumerable<int> CommentIds { get; set; }     
-        public IEnumerable<int> AttendingUserIds { get; set; }
+        //public IEnumerable<int> ImageIds { get; set; }    
+        //public IEnumerable<int> CommentIds { get; set; }     
+        //public IEnumerable<int> AttendingUserIds { get; set; }
     }
 }
