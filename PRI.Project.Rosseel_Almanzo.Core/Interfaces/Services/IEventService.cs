8ba8155ac309f9bdcaf9084754f7f097577aad7d
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-26 13:30:59 +0100

    Add new method checkifexistsasync in interface ieventservice

--- PRI.Project.Rosseel_Almanzo.Core/Interfaces/Services/IEventService.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Interfaces/Services/IEventService.cs
@@ -16,5 +16,6 @@ namespace PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services
         Task<ResultModel<Event>> CreateEventAsync(EventCreateRequestModel eventCreateRequestModel);
         //Task<ResultModel<Event>> UpdateRecordAsync(EventUpdateRequestModel eventUpdateRequestModel);
         Task<ResultModel<Event>> DeleteEventAsync(int id);
+        Task<bool> CheckIfExistsAsync(int id);
     }
 }
