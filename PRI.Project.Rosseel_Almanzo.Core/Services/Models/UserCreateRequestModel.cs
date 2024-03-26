6aa80d7fc17138f5c27ffd574e2ead074b218445
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-26 21:07:29 +0100

    Create new model usercreaterequestmodel

---
+++ PRI.Project.Rosseel_Almanzo.Core/Services/Models/UserCreateRequestModel.cs
@@ -0,0 +1,22 @@
+﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Services.Models
+{
+    public class UserCreateRequestModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Address Address { get; set; }
+
+        //public IEnumerable<Dog> Dogs { get; set;}       
+    }
+}
