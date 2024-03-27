d1e1e34926d65c1c0112bf76d8ccbfb00ca8424b
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-03-27 10:23:25 +0100

    Create entity eventuser

---
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/EventUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Entities
+{
+    public class EventUser
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+    }
+}
