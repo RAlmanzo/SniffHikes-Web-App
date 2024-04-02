3b088bf93aa9badc6c4b775d2151474be40aa0b4
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-02 19:43:56 +0200

    Create dogrequestdto

---
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/DogRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
+{
+    public class DogRequestDto
+    {
+        [Required(ErrorMessage = "Dog name is required")]
+        [StringLength(50, ErrorMessage = "Dog name is too long")]
+        public string Value { get; set; }
+        public string Gender { get; set; }
+        public string Race { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Image {  get; set; }
+
+        [HiddenInput]
+        public int UserId { get; set; }
+    }
+}
