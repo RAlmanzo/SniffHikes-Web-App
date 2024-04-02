026d09fcd82d825c7c74716f1840b2f6fce1e60a
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-02 19:44:40 +0200

    Add input checks on properties

--- PRI.Project.Rosseel_Almanzo.Api/Dtos/UserRequestDto.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/UserRequestDto.cs
@@ -1,26 +1,38 @@
-﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+﻿using Microsoft.AspNetCore.Mvc;
+using PRI.Project.Rosseel_Almanzo.Core.Entities;
 using System.ComponentModel.DataAnnotations;
 
 namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
 {
     public class UserRequestDto
     {
+        [HiddenInput]
+        public int Id { get; set; }
+
         [Required(ErrorMessage = "Firstname is required")]
-        [StringLength(50, ErrorMessage = "Firstname is to long")]
+        [StringLength(50, ErrorMessage = "Firstname is too long")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Lastname is required")]
-        [StringLength(50, ErrorMessage = "Lastname is to long")]
+        [StringLength(50, ErrorMessage = "Lastname is too long")]
         public string LastName { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Please enter a valid date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateOfBirth { get; set; }
+
         public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
-        public Address Address { get; set; }
-        public IEnumerable<int> CommentIds { get; set; }
-        public IEnumerable<int> OrganizedEventIds { get; set; }
-        public IEnumerable<int> AttendingEventIds { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public AddressRequestDto Address { get; set; }
         public IEnumerable<int> ImageIds { get; set; }
-        //public IEnumerable<Dog> Dogs { get; set; }
+        public IEnumerable<DogRequestDto> Dogs { get; set; }
     }
 }
