c250b974c1d8604f4dd5e5867104fc2b62460491
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-05 13:42:42 +0200

    create ieventuserrepository

---
+++ PRI.Project.Rosseel_Almanzo.Core/Interfaces/Repositories/IEventUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Interfaces.Repositories
+{
+    public interface IEventUserRepository
+    {
+        Task<IEnumerable<EventUser>> GetAllByUserId(int id);
+        Task<bool> DeleteAsync(EventUser toDelete);
+    }
+}
