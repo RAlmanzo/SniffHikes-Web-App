9cef5933bf8f21b77cbbd520798d2b6a013f9080
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 17:57:45 +0200

    Create new api controller routescontroller and edit getall action method

---
+++ PRI.Project.Rosseel_Almanzo.Api/Controllers/RoutesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PRI.Project.Rosseel_Almanzo.Api.Extensions;
+using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services;
+
+namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoutesController : ControllerBase
+    {
+        private readonly IRouteService _routeService;
+
+        public RoutesController(IRouteService routeService)
+        {
+            _routeService = routeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            //get all events
+            var result = await _routeService.GetAllAsync();
+            //check if result is succes
+            if (result.Success)
+            {
+                return Ok(result.Value.MapToDto());
+            }
+            return NotFound(result.Errors);
+        }
+    }
+}
