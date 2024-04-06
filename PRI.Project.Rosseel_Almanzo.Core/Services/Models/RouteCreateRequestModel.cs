5f0f1bbcd4c9c81b107d7aceb3578a194a7160ba
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 19:03:32 +0200

    Create routecreaterequestmodel

---
+++ PRI.Project.Rosseel_Almanzo.Core/Services/Models/RouteCreateRequestModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Services.Models
+{
+    public class RouteCreateRequestModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public int OrganizerId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public IEnumerable<string> Images { get; set; }
+        //public string Image { get; set; }
+    }
+}
