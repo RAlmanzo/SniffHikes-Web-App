da7c6867bd926dfabc9d3572931488976a5e4fef
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 16:01:08 +0200

    Refacter code to implement fileservice to store or delete image from event

--- PRI.Project.Rosseel_Almanzo.Api/Controllers/EventsController.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Controllers/EventsController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PRI.Project.Rosseel_Almanzo.Api.Dtos;
 using PRI.Project.Rosseel_Almanzo.Api.Extensions;
+using PRI.Project.Rosseel_Almanzo.Api.Services.Interfaces;
+using PRI.Project.Rosseel_Almanzo.Core.Entities;
 using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services;
+using PRI.Project.Rosseel_Almanzo.Core.Services;
 using PRI.Project.Rosseel_Almanzo.Core.Services.Models;
+using System;
 
 namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
 {
@@ -13,12 +17,14 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
         private readonly IEventService _eventService;
         private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly ILogger<EventsController> _logger;
+        private readonly IFileService _fileService;
 
-        public EventsController(IEventService eventService, IWebHostEnvironment webHostEnvironment, ILogger<EventsController> logger)
+        public EventsController(IEventService eventService, IWebHostEnvironment webHostEnvironment, ILogger<EventsController> logger, IFileService fileService)
         {
             _eventService = eventService;
             _webHostEnvironment = webHostEnvironment;
             _logger = logger;
+            _fileService = fileService;
         }
 
         [HttpGet]
@@ -48,8 +54,26 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Add(EventRequestDto eventRequestDto)
+        public async Task<IActionResult> Add([FromForm]EventRequestDto eventRequestDto)
         {
+            //check if image is present
+            var filename = "";
+            if (eventRequestDto.Image != null)
+            {
+                filename = await _fileService.StoreFile<Event>(eventRequestDto.Image);
+            }
+
+            ////Dit is de code die ik zou gebruiken als ik een list van images kan meegeven in swagger!!!
+            ////check if images are given
+            //var filenames = "";
+            //if (eventRequestDto.ImageUrls != null)
+            //{
+            //    foreach (var url in eventRequestDto.ImageUrls)
+            //    {
+            //        filename += await _fileService.StoreFile<Event>(url);
+            //    }
+            //}
+
             var result = await _eventService.CreateEventAsync(
                 new EventCreateRequestModel
                 {
@@ -62,7 +86,7 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
                     Country = eventRequestDto.Address.Country,
                     OrganizerId = eventRequestDto.OrganizerId,
                     Date = eventRequestDto.Date,
-                    //TODO images
+                    Image = filename
                 });
 
             if (result.Success)
@@ -81,11 +105,24 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            if (!await _eventService.CheckIfExistsAsync(id))
+            var eventResult = await _eventService.GetByIdAsync(id);
+
+            if (!eventResult.Success)
             {
-                return NotFound("Event not found!");
+                return NotFound("User not found!");
             }
 
+            foreach(var image in eventResult.Value.Images)
+            {
+                if (!string.IsNullOrWhiteSpace(image.File))
+                {
+                    if (!_fileService.DeleteFile<User>(image.File))
+                    {
+                        ModelState.AddModelError("", "Image not found");
+                    }
+                }
+            }
+            
             var result = await _eventService.DeleteEventAsync(id);
             if (result.Success)
             {
@@ -100,42 +137,64 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update(EventUpdateRequestDto eventUpdateRequestDto)
+        public async Task<IActionResult> Update([FromForm]EventUpdateRequestDto eventUpdateRequestDto)
         {
             //check if event exists
             if (!await _eventService.CheckIfExistsAsync(eventUpdateRequestDto.Id))
             {
                 return NotFound("Event not found!");
             }
+            
+            //check if new user image
+            var filename = "";
+            if (eventUpdateRequestDto.Image != null)
+            {
+                //get event
+                var selectedEvent = await _eventService.GetByIdAsync(eventUpdateRequestDto.Id);
+                //delete current image
+                foreach (var image in selectedEvent.Value.Images)
+                {
+                    if (!string.IsNullOrWhiteSpace(image.File))
+                    {
+                        if (!_fileService.DeleteFile<User>(image.File))
+                        {
+                            ModelState.AddModelError("", "Image not found");
+                        }
+                    }
+                }             
+                //save new image
+                filename = await _fileService.StoreFile<User>(eventUpdateRequestDto.Image);
+            }
 
             var result = await _eventService.UpdateEventAsync
-                (
-                    new EventUpdateRequestModel
-                    {
-                        Id = eventUpdateRequestDto.Id,
-                        Title = eventUpdateRequestDto.Title,
-                        Description = eventUpdateRequestDto.Description,
-                        Price = eventUpdateRequestDto.Price,
-                        Street = eventUpdateRequestDto.Address.Street,
-                        City = eventUpdateRequestDto.Address.City,
-                        State = eventUpdateRequestDto.Address.State,
-                        Country = eventUpdateRequestDto.Address.Country,
-                        OrganizerId = eventUpdateRequestDto.OrganizerId,
-                        Date = eventUpdateRequestDto.Date,
-                        DateCreated = eventUpdateRequestDto.DateCreated,
-                        //ImageIds = eventUpdateRequestDto.ImageIds,
-                        //CommentIds = eventUpdateRequestDto.CommentIds,
-                        //AttendingUserIds = eventUpdateRequestDto.AttendingUserIds,
-                    }
-                );
+            (
+                new EventUpdateRequestModel
+                {
+                    Id = eventUpdateRequestDto.Id,
+                    Title = eventUpdateRequestDto.Title,
+                    Description = eventUpdateRequestDto.Description,
+                    Price = eventUpdateRequestDto.Price,
+                    Street = eventUpdateRequestDto.Address.Street,
+                    City = eventUpdateRequestDto.Address.City,
+                    State = eventUpdateRequestDto.Address.State,
+                    Country = eventUpdateRequestDto.Address.Country,
+                    OrganizerId = eventUpdateRequestDto.OrganizerId,
+                    Date = eventUpdateRequestDto.Date,
+                    //ImageIds = eventUpdateRequestDto.ImageIds,
+                    Image = filename,
+                }
+            );
+
             if (result.Success)
             {
                 return Ok();
             }
+
             foreach (var error in result.Errors)
             {
                 ModelState.AddModelError("", error);
             }
+
             return BadRequest(ModelState.Values);
         }
     }
