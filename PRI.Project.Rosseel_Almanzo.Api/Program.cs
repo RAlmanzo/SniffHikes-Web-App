dcb481d598159d456b395f61bc753126e1c19bdb
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 23:17:34 +0200

    Add builder service for iimageservice

--- PRI.Project.Rosseel_Almanzo.Api/Program.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Program.cs
@@ -28,6 +28,7 @@ namespace PRI.Project.Rosseel_Almanzo.Api
             builder.Services.AddScoped<IAddressRepository, AddressRepository>();
             builder.Services.AddScoped<IEventUserRepository, EventUserRepository>();
             builder.Services.AddScoped<IImageRepository, ImageRepository>();
+            builder.Services.AddScoped<IImageService, ImageService>();
             builder.Services.AddScoped<IRouteRepository, RouteRepository>();
             builder.Services.AddScoped<IRouteService, RouteService>();
             builder.Services.AddScoped<ICommentRepository, CommentRepository>();
