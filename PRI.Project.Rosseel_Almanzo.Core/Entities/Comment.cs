2e4fd46fba26853de47a775bf7e8d4875d102f08
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 19:54:37 +0200

    Refacter entity comment

--- PRI.Project.Rosseel_Almanzo.Core/Entities/Comment.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Entities/Comment.cs
@@ -1,8 +1,7 @@
 ﻿namespace PRI.Project.Rosseel_Almanzo.Core.Entities
 {
-    public class Comment
+    public class Comment : BaseEntity
     {
-        public int Id { get; set; }
         public string Content { get; set; }
         public DateTime DateCreated { get; set; }
         public User User { get; set; }
