01c08234a42165020f45bd9185a36135fc3f1992
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 15:57:34 +0200

    Add property string image to eventcreaterequestmodel

--- PRI.Project.Rosseel_Almanzo.Core/Services/Models/EventCreateRequestModel.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Services/Models/EventCreateRequestModel.cs
@@ -18,6 +18,8 @@ namespace PRI.Project.Rosseel_Almanzo.Core.Services.Models
         public string Country { get; set; }
         public int OrganizerId { get; set; }
         public DateTime Date { get; set; }
-        public DateTime DateCreated { get; set; }    
+        public DateTime DateCreated { get; set; }
+        //public IEnumerable<string> ImageUrls { get; set; }
+        public string Image {  get; set; }
     }
 }
