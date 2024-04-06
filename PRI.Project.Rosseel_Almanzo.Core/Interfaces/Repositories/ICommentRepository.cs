ea3fbd96bfd31e33bced24c7a98edfd36d3ec494
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 19:52:37 +0200

    Create icommentrepository interface

---
+++ PRI.Project.Rosseel_Almanzo.Core/Interfaces/Repositories/ICommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Interfaces.Repositories
+{
+    public interface ICommentRepository : IBaseRepository<Comment>
+    {
+    }
+}
