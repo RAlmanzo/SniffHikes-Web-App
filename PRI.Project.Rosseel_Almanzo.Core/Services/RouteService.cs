bf7ffd1b6135e2db807347dd13388322cf5356b7
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 17:59:56 +0200

    Create routeservice

---
+++ PRI.Project.Rosseel_Almanzo.Core/Services/RouteService.cs
@@ -0,0 +1,50 @@
+﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Repositories;
+using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services;
+using PRI.Project.Rosseel_Almanzo.Core.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Services
+{
+    public class RouteService : IRouteService
+    {
+        private readonly IRouteRepository _routeRepository;
+
+        public RouteService(IRouteRepository routeRepository)
+        {
+            _routeRepository = routeRepository;
+        }
+
+        public Task<ResultModel<Route>> DeleteRouteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ResultModel<IEnumerable<Route>>> GetAllAsync()
+        {
+            //get the routes
+            var routes = await _routeRepository.GetAllAsync();
+            //create new resultmodel
+            var eventResultModel = new ResultModel<IEnumerable<Route>>();
+            //check if count > 0
+            if (routes.Count() > 0)
+            {
+                eventResultModel.Success = true;
+                eventResultModel.Value = routes;
+                return eventResultModel;
+            }
+            //if not
+            eventResultModel.Errors = new List<string> { "No events found" };
+            return eventResultModel;
+        }
+
+        public Task<ResultModel<Route>> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
