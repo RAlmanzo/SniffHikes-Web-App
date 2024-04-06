5756dbf74ece331745b38731f9e61865a5b15d99
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 20:51:11 +0200

    Create routeupdaterequestmodel

---
+++ PRI.Project.Rosseel_Almanzo.Core/Services/Models/RouteUpdateRequestModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Services.Models
+{
+    public class RouteUpdateRequestModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public int OrganizerId { get; set; }
+    }
+}
