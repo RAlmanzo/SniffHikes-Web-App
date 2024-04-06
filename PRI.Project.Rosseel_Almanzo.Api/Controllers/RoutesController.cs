bf9e6e2e86f5c38e62db002fbeec58e0628bbde5
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 18:18:50 +0200

    Create new action method get

--- PRI.Project.Rosseel_Almanzo.Api/Controllers/RoutesController.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Controllers/RoutesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PRI.Project.Rosseel_Almanzo.Api.Extensions;
 using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services;
+using PRI.Project.Rosseel_Almanzo.Core.Services;
 
 namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
 {
@@ -21,6 +22,19 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
         {
             //get all events
             var result = await _routeService.GetAllAsync();
+            //check if result is succes
+            if (result.Success)
+            {
+                return Ok(result.Value.MapToDto());
+            }
+            return NotFound(result.Errors);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            //get the record
+            var result = await _routeService.GetByIdAsync(id);
             //check if result is succes
             if (result.Success)
             {
