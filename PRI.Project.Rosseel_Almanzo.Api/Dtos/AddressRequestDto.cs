5d479719485885dbc23db48e965f640fdea18d44
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-06 15:52:58 +0200

    Add stringlenght requirements for addressrequestdto

--- PRI.Project.Rosseel_Almanzo.Api/Dtos/AddressRequestDto.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/AddressRequestDto.cs
@@ -4,12 +4,19 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
 {
     public class AddressRequestDto
     {
-        [StringLength(50, ErrorMessage = "Country name is too long")]
+        [Required(ErrorMessage = "Street is required")]
+        [StringLength(50, ErrorMessage = "Street name is too long")]
         public string Street { get; set; }
-        [StringLength(50, ErrorMessage = "Country name is too long")]
+
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(50, ErrorMessage = "City name is too long")]
         public string City { get; set; }
-        [StringLength(50, ErrorMessage = "Country name is too long")]
+
+        [Required(ErrorMessage = "State is required")]
+        [StringLength(50, ErrorMessage = "State name is too long")]
         public string State { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
         [StringLength(50, ErrorMessage = "Country name is too long")]
         public string Country { get; set; }
     }
