56541168a7d75ecf7c10638c007d9a8e9985d915
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-07 22:43:29 +0200

    Add new actionmethod searchbytitle

--- PRI.Project.Rosseel_Almanzo.Api/Controllers/EventsController.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Controllers/EventsController.cs
@@ -225,5 +225,20 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
             }
             return BadRequest(ModelState.Values);
         }
+
+        [HttpGet("Search/ByTitle/{title}")]
+        public async Task<IActionResult> SearchByTitle(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                return BadRequest("character not allowed!");
+            }
+            var result = await _eventService.SearchByTitleAsync(title);
+            if (result.Success)
+            {
+                return Ok(result.Value.MapToDto());
+            }
+            return Ok(result.Errors);
+        }
     }
 }
