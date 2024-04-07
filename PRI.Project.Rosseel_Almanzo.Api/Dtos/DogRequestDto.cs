cf85225b8e7f0827e44f4f2510887aed1793bdb9
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-07 17:52:23 +0200

    Refacter dogrequestdto

--- PRI.Project.Rosseel_Almanzo.Api/Dtos/DogRequestDto.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/DogRequestDto.cs
@@ -5,6 +5,12 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
 {
     public class DogRequestDto
     {
-        public IEnumerable<BaseDogRequestDto> Dogs { get; set; }
+        [Required(ErrorMessage = "Dog name is required")]
+        [StringLength(50, ErrorMessage = "Dog name is too long")]
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Race { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public IFormFile Image { get; set; }
     }
 }
