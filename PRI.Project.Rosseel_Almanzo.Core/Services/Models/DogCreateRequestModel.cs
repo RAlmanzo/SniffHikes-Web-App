c27d52fbdb545111c440521866745b052e5fa0d6
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-07 17:54:38 +0200

    Create dogcreaterequestmodel

---
+++ PRI.Project.Rosseel_Almanzo.Core/Services/Models/DogCreateRequestModel.cs
@@ -0,0 +1,19 @@
+﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRI.Project.Rosseel_Almanzo.Core.Services.Models
+{
+    public class DogCreateRequestModel
+    {
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Image { get; set; }
+        public int UserId { get; set; }
+    }
+}
