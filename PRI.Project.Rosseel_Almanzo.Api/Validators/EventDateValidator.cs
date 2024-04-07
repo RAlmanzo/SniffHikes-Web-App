3a8051ccd930c2a451d5694575a32951e13868f4
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-07 14:35:14 +0200

    Create validationattribute eventdatevalidator

---
+++ PRI.Project.Rosseel_Almanzo.Api/Validators/EventDateValidator.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRI.Project.Rosseel_Almanzo.Api.Validators
+{
+    public class EventDateValidator : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is DateTime date)
+            {
+                return date >= DateTime.Now;
+            }
+
+            return false;
+        }
+    }
+}
