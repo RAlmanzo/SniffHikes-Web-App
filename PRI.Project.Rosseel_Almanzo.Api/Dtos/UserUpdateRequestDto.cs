d4a8b45aa1c560e027777e822ca5b79f2a9429d2
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-07 14:39:35 +0200

    Update requestdtos for user

--- PRI.Project.Rosseel_Almanzo.Api/Dtos/UserUpdateRequestDto.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/UserUpdateRequestDto.cs
@@ -1,31 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using PRI.Project.Rosseel_Almanzo.Api.Validators;
+using System.ComponentModel.DataAnnotations;
 
 namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
 {
-    public class UserUpdateRequestDto
+    public class UserUpdateRequestDto : UserRequestDto
     {
         [Required(ErrorMessage = "Id required")]
         [Range(1, int.MaxValue)]
         public int Id { get; set; }
-
-        [StringLength(50, ErrorMessage = "Firstname is too long")]
-        public string FirstName { get; set; }
-
-        [StringLength(50, ErrorMessage = "Lastname is too long")]
-        public string LastName { get; set; }
-        [DataType(DataType.Date, ErrorMessage = "Please enter a valid date")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime DateOfBirth { get; set; }
-
-        public string Gender { get; set; }
-
-        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
-        public string Email { get; set; }
-
-        public string Password { get; set; }
-
-        public AddressRequestDto Address { get; set; }
-        //public IEnumerable<BaseDogRequestDto> Dogs { get; set; }
-        public IFormFile Image { get; set; }
     }
 }
