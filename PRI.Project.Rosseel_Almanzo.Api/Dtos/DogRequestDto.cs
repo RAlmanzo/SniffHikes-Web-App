f81990b8f5a382ed749e5ce8cc0b8760005aa01f
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-07 20:44:35 +0200

    Add validation on dateofbirth of dog

--- PRI.Project.Rosseel_Almanzo.Api/Dtos/DogRequestDto.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/DogRequestDto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PRI.Project.Rosseel_Almanzo.Api.Validators;
 using System.ComponentModel.DataAnnotations;
 
 namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
@@ -10,6 +11,10 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
         public string Name { get; set; }
         public string Gender { get; set; }
         public string Race { get; set; }
+        [Required(ErrorMessage = "Date of birth is required")]
+        [DataType(DataType.Date, ErrorMessage = "Please enter a valid date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [UserDateValidator(ErrorMessage = "Birthday cant be in de future")]
         public DateTime DateOfBirth { get; set; }
         public IFormFile Image { get; set; }
     }
