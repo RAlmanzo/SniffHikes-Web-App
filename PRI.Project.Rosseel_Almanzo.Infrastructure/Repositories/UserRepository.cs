59789fa22aed5fcbf47f7dc1efa6c3b16225bd61
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-21 12:09:35 +0200

    Refacter code to use identity

--- PRI.Project.Rosseel_Almanzo.Infrastructure/Repositories/UserRepository.cs
+++ PRI.Project.Rosseel_Almanzo.Infrastructure/Repositories/UserRepository.cs
@@ -11,13 +11,37 @@ using System.Threading.Tasks;
 
 namespace PRI.Project.Rosseel_Almanzo.Infrastructure.Repositories
 {
-    public class UserRepository : BaseRepository<User>, IUserRepository
+    public class UserRepository : IUserRepository
     {
-        public UserRepository(SniffHikesDbContext context, ILogger<IBaseRepository<User>> logger) : base(context, logger)
+        private readonly SniffHikesDbContext _dbContext;
+        protected readonly DbSet<User> _targetTable;
+        private readonly ILogger<IUserRepository> _logger;
+
+        public UserRepository(SniffHikesDbContext dbContext, ILogger<IUserRepository> logger)
         {
+            _dbContext = dbContext;
+            _targetTable = _dbContext.Set<User>();
+            _logger = logger;
         }
 
-        public override async Task<IEnumerable<User>> GetAllAsync()
+        public async Task<bool> AddAsync(User toAdd)
+        {
+            _targetTable.Add(toAdd);
+            return await SaveChangesAsync();
+        }
+
+        public async Task<bool> DeleteAsync(User toDelete)
+        {
+            _targetTable.Remove(toDelete);
+            return await SaveChangesAsync();
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return _targetTable.AsQueryable();
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
         {
             var data = await _targetTable
                 .Include(u => u.Address)
@@ -30,14 +54,7 @@ namespace PRI.Project.Rosseel_Almanzo.Infrastructure.Repositories
             return data;
         }
 
-        public IQueryable<Dog> GetAllUserDogs(int id)
-        {
-            var user = GetByIdAsync(id);
-
-            return user.Result.Dogs.AsQueryable();
-        }
-
-        public override async Task<User> GetByIdAsync(int id)
+        public async Task<User> GetByIdAsync(string id)
         {
             return await _targetTable
                 .Include(u => u.Address)
@@ -47,6 +64,38 @@ namespace PRI.Project.Rosseel_Almanzo.Infrastructure.Repositories
                 .Include(u => u.OrganizedEvents)
                 .Include(u => u.AttendingEvents)
                 .FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<bool> UpdateAsync(User toUpdate)
+        {
+            _targetTable.Update(toUpdate);
+            return await SaveChangesAsync();
+        }
+
+        public IQueryable<Dog> GetAllUserDogs(string id)
+        {
+            var user = GetByIdAsync(id);
+
+            return user.Result.Dogs.AsQueryable();
+        }
+
+        private async Task<bool> SaveChangesAsync()
+        {
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                _logger.LogError(dbUpdateException.Message);
+                return false;
+            }
+        }
+
+        public async Task<bool> CheckIfExistsAsync(string id)
+        {
+            return await _targetTable.AnyAsync(t => t.Id == id);
         }
     }
 }
