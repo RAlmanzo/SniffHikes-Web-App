8093348a2dbdb08166d9a89cf3a89de8f4bc0446
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-21 14:31:04 +0200

    Create authloginrequestdto

---
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/AuthLoginRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
+{
+    public class AuthLoginRequestDto
+    {
+        [Required]
+        [EmailAddress]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
