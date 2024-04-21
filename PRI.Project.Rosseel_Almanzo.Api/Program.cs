6ca0f0b958024f2343e8867d176337682112d1fb
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-21 20:04:10 +0200

    Add options to swagger to be able to login

--- PRI.Project.Rosseel_Almanzo.Api/Program.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PRI.Project.Rosseel_Almanzo.Api.Services;
 using PRI.Project.Rosseel_Almanzo.Api.Services.Interfaces;
 using PRI.Project.Rosseel_Almanzo.Core.Entities;
@@ -122,7 +123,32 @@ namespace PRI.Project.Rosseel_Almanzo.Api
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(option =>
+            {
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                 {
+                     {
+                           new OpenApiSecurityScheme
+                             {
+                                 Reference = new OpenApiReference
+                                 {
+                                     Type = ReferenceType.SecurityScheme,
+                                     Id = "Bearer"
+                                 }
+                             },
+                             new string[] {}
+                     }
+                 });
+            });
 
             var app = builder.Build();
 
@@ -130,7 +156,7 @@ namespace PRI.Project.Rosseel_Almanzo.Api
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "JWTAuthDemo v1"));
             }
 
             app.UseHttpsRedirection();
