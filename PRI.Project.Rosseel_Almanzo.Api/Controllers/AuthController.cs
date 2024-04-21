a1a7bfdc84b037b3a6177af1161916d020ab6ac9
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-21 20:01:53 +0200

    Add new api endpoint register

--- PRI.Project.Rosseel_Almanzo.Api/Controllers/AuthController.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Controllers/AuthController.cs
@@ -3,8 +3,12 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using PRI.Project.Rosseel_Almanzo.Api.Dtos;
+using PRI.Project.Rosseel_Almanzo.Api.Services.Interfaces;
 using PRI.Project.Rosseel_Almanzo.Core.Entities;
+using PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services;
+using PRI.Project.Rosseel_Almanzo.Core.Services.Models;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
@@ -16,12 +20,16 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         private readonly IConfiguration _configuration;
+        private readonly IUserService _userService;
+        private readonly IFileService _fileService;
 
-        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration configuration)
+        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration configuration, IUserService userService, IFileService fileService)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _configuration = configuration;
+            _userService = userService;
+            _fileService = fileService;
         }
 
         [HttpPost("Login")]
@@ -60,6 +68,51 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
             var serializedToken = new JwtSecurityTokenHandler().WriteToken(token);
             //return the token
             return Ok(new AuthLoginResponseDto { Token = serializedToken });
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromForm]AuthRegisterRequestDto authRegisterRequestDto)
+        {
+            //create the user
+            //check if image is given
+            var filename = "";
+            if (authRegisterRequestDto.Image != null)
+            {
+                filename = await _fileService.StoreFile<User>(authRegisterRequestDto.Image);
+            }
+
+            var result = await _userService.CreateUserAsync(
+                new UserCreateRequestModel
+                {
+                    FirstName = authRegisterRequestDto.FirstName,
+                    LastName = authRegisterRequestDto.LastName,
+                    DateOfBirth = authRegisterRequestDto.DateOfBirth,
+                    Gender = authRegisterRequestDto.Gender,
+                    Email = authRegisterRequestDto.Email,
+                    Password = authRegisterRequestDto.Password,
+                    Image = filename,
+                    Address = new Address
+                    {
+                        Street = authRegisterRequestDto.Address.Street,
+                        City = authRegisterRequestDto.Address.City,
+                        State = authRegisterRequestDto.Address.State,
+                        Country = authRegisterRequestDto.Address.Country,
+                    },
+
+                });
+
+            if (!result.Success)
+            {
+                //add to the modelstate
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error);
+                }
+                return BadRequest(ModelState.Values);
+            }
+
+            //call the emailservice
+            return Ok("Registered");
         }
     }
 }
