d578d4656357e37679a730b19c96c3a5a4b7e684
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-21 20:04:54 +0200

    Change types of methods deleteasync and addasync to identityresult

--- PRI.Project.Rosseel_Almanzo.Core/Interfaces/Repositories/IUserRepository.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Interfaces/Repositories/IUserRepository.cs
@@ -1,4 +1,5 @@
-﻿using PRI.Project.Rosseel_Almanzo.Core.Entities;
+﻿using Microsoft.AspNetCore.Identity;
+using PRI.Project.Rosseel_Almanzo.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +13,10 @@ namespace PRI.Project.Rosseel_Almanzo.Core.Interfaces.Repositories
         Task<User> GetByIdAsync(string id);
         IQueryable<User> GetAll();
         Task<IEnumerable<User>> GetAllAsync();
-        Task<bool> DeleteAsync(User toDelete);
-        Task<bool> AddAsync(User toAdd);
+        //Task<bool> DeleteAsync(User toDelete);
+        Task<IdentityResult> DeleteAsync(User toDelete);
+        //Task<bool> AddAsync(User toAdd);
+        Task<IdentityResult> AddAsync(User toAdd);
         Task<bool> UpdateAsync(User toUpdate);
         Task<bool> CheckIfExistsAsync(string id);
         IQueryable<Dog> GetAllUserDogs(string id);
