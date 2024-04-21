bef0bc5854774f45fc2b076c1a8ee87b92dbb480
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-21 20:02:31 +0200

    Create authregisterrequestdto

---
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/AuthRegisterRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
+{
+    public class AuthRegisterRequestDto : UserRequestDto
+    {       
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string RepeatPassword { get; set; }
+    }
+}
