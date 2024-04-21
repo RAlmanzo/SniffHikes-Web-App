3ca40586ed4873a71bfaa7b14f9802dd4e76be66
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-21 12:19:34 +0200

    Inject usermanager signinmanager and configuration in authcontroller

--- PRI.Project.Rosseel_Almanzo.Api/Controllers/AuthController.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using PRI.Project.Rosseel_Almanzo.Core.Entities;
 
 namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
 {
@@ -7,5 +9,15 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
     }
 }
