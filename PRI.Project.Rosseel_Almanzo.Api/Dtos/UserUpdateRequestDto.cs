6620b562a0369c4a87b37b97ff79437c4c5febad
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-29 13:30:16 +0200

    Remove range on id

--- PRI.Project.Rosseel_Almanzo.Api/Dtos/UserUpdateRequestDto.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/UserUpdateRequestDto.cs
@@ -6,7 +6,6 @@ namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
     public class UserUpdateRequestDto : UserRequestDto
     {
         [Required(ErrorMessage = "Id required")]
-        [Range(1, int.MaxValue)]
         public string Id { get; set; }
     }
 }
