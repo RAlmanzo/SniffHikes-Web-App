dd3d99d3dfdf917a66e96f7c4556db08087eab64
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-01 16:42:46 +0200

    Create new dto userresetpasswordrequestdto

---
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/UserResetPasswordRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
+{
+    public class UserResetPasswordRequestDto
+    {
+        public string currentPassword { get; set; }
+        public string newPassword { get; set; }
+    }
+}
