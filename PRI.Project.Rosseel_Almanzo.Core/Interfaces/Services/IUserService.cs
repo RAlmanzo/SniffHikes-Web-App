cf185eb6884d50792fd6dba00118fd8ae3361fb6
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-01 14:40:50 +0200

    Change parameter username to email

--- PRI.Project.Rosseel_Almanzo.Core/Interfaces/Services/IUserService.cs
+++ PRI.Project.Rosseel_Almanzo.Core/Interfaces/Services/IUserService.cs
@@ -19,7 +19,7 @@ namespace PRI.Project.Rosseel_Almanzo.Core.Interfaces.Services
         Task<bool> CheckIfExistsAsync(string id);
         Task<ResultModel<IEnumerable<User>>> SearchByFirstNameAsync(string firstName);
         Task<ResultModel<IEnumerable<User>>> SearchByLastNameAsync(string lastName);
-        Task<ResultModel<string>> LoginUserAsync(string userName, string password);
+        Task<ResultModel<string>> LoginUserAsync(string email, string password);
         Task<bool> SignOutUserAsync();
     }
 }
