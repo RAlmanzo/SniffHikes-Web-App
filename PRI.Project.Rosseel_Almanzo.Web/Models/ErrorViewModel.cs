0396024e49087ef250d7da773d8c935a1cd7b0b5
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-13 14:04:48 +0200

    Add new mvc project in solution

---
+++ PRI.Project.Rosseel_Almanzo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRI.Project.Rosseel_Almanzo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
