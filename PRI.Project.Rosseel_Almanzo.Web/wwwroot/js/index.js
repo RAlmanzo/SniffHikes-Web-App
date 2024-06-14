d58f7179ca71ff62952ee08a7cbf20f5482b43ef
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-15 00:10:59 +0200

    Create index js file

---
+++ PRI.Project.Rosseel_Almanzo.Web/wwwroot/js/index.js
@@ -0,0 +1,22 @@
+﻿var indexVue = new Vue({
+    el: "#index",
+    name: "indexbtns",
+    data: {
+        isAdmin: false,
+        isUser: false,
+    },
+    methods: {
+        checkClaims: async function () {
+            const token = sessionStorage.getItem("token");
+
+            if (token !== null) {
+                this.isAdmin = hasUserAdminRole();
+                this.isUser = !this.isAdmin;
+            }
+        },
+        resetData: function () {
+            this.isAdmin = false;
+            this.isUser = false;
+        },
+    }
+});
