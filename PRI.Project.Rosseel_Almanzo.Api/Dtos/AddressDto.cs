922279c8abb8bc980756a52c624ac3da2bc8f17f
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-16 04:12:48 +0200

    Create addressdto

---
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/AddressDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRI.Project.Rosseel_Almanzo.Api.Dtos
+{
+    public class AddressDto
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
