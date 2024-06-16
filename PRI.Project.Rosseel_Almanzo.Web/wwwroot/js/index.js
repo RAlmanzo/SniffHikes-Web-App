28775feeeb342feca4959342ea0ffdf0f050585a
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-16 21:25:00 +0200

    Add created and call checkclaims

--- PRI.Project.Rosseel_Almanzo.Web/wwwroot/js/index.js
+++ PRI.Project.Rosseel_Almanzo.Web/wwwroot/js/index.js
@@ -5,12 +5,15 @@
         isAdmin: false,
         isUser: false,
     },
+    created: function () {
+        this.checkClaims();
+    },
     methods: {
         checkClaims: async function () {
             const token = sessionStorage.getItem("token");
 
             if (token !== null) {
-                this.isAdmin = hasUserAdminRole();
+                this.isAdmin = hasAdminRole();
                 this.isUser = !this.isAdmin;
             }
         },
