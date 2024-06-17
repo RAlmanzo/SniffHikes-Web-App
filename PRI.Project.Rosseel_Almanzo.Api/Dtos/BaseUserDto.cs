f430dea584a260a1d6c7f3a174b78778753db37b
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-17 02:29:48 +0200

    Add properties firstname and lastname to baseuserdto

--- PRI.Project.Rosseel_Almanzo.Api/Dtos/BaseUserDto.cs
+++ PRI.Project.Rosseel_Almanzo.Api/Dtos/BaseUserDto.cs
@@ -4,6 +4,8 @@
     {
         public string Id { get; set; }
         public string Value { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Image {  get; set; }
     }
 }
